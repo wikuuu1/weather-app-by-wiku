d355d8e90a7b66269dc3822048c49871460f50d7
wikuuu1 <wiktoria.rajba391@gmail.com>
2021-04-21 22:23:44 +0200

    all time changes in html

--- temperature.js
+++ temperature.js
@@ -1,14 +1,27 @@
 function displayWeather(response) {
 
   let cityElement = document.querySelector("#localization");
-  cityElement.innerHTML = response.data.name;
+  let humidityElement = document.querySelector("#actuallHumidity");
 
+
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
 
 }
 
+function search(city) {
+  let apiKey = "af7eeef60a1b879ba0783c7aa018fc95";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
 
-let apiKey = "af7eeef60a1b879ba0783c7aa018fc95";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeather);
+}
 
-axios.get(apiUrl).then(displayWeather);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#currentCity");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
